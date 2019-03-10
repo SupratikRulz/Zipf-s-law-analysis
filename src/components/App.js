d995534ab558af5145edd9d3c49afab78e2ff902
Supratik <supratik@fusioncharts.com>
2019-03-10 12:11:56 +0530

    Calculation of word frequency on the fly implemented

--- src/components/App.js
+++ src/components/App.js
@@ -9,21 +9,42 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      data: []
+      data: [],
+      wordFreqArr: [],
+      wordCount: 0
     }
   }
 
 
   componentDidMount() {
+    let wordFrequencyObj = {},
+      wordFreqArr,
+      wordCount;
     this.setState({
       data: scrapedData
     })
+
+    scrapedData.forEach(word => {
+      let frequency = wordFrequencyObj[word];
+      wordFrequencyObj[word] =  frequency ? frequency + 1 : 1;
+    });
+
+    wordFreqArr = Object.keys(wordFrequencyObj)
+                      .map(key => ({name: key, frequency: wordFrequencyObj[key]}))
+                      .sort((objA, objB) => objB.frequency - objA.frequency);
+
+    wordCount = wordFreqArr.length;
+
+    this.setState({wordFreqArr, wordCount})
+
+
+
   }
 
    render() {
     return (
       <div className="App">
-        {this.state.data.map(word => <div>{word}</div>)}
+        {this.state.wordFreqArr.map(word => <div>{word["name"]}&nbsp; {word["frequency"]}</div>)}
       </div>
     );
   }
